4805c72fd45172905b5679e1dc86274d762efe56
Devil7DK <dineshthangavel47@gmail.com>
2020-12-13 20:11:41 +0530

    Install and setup storybook

---
+++ src/stories/DrifterStars.stories.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from 'react';
+
+import { DrifterStars } from '../DrifterStars';
+
+export default { title: 'Drifter Stars' };
+
+export const basic = (): ReactNode => <DrifterStars />;
