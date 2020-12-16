4ac83a8b80ce680dd91bcf68e328fb503f01b5b0
Devil7DK <dineshthangavel47@gmail.com>
2020-12-16 10:57:39 +0530

    Add theme for story book

---
+++ .storybook/customTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'dark',
+
+    brandTitle: 'Devil7 Softwares',
+    brandUrl: 'https://storybook.devil7softwares.com',
+    brandImage: 'https://devil7softwares.com/images/logo_230x150.png',
+});
